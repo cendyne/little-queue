6392f06118c14e5f408596dc8c07c6071302e0b1
Cendyne <cendyne@cendyne.dev>
2021-09-04 15:50:21 -0500

    Docker hub workflow

---
+++ dockerhub.sh
@@ -0,0 +1,27 @@
+PLATFORMS="linux/amd64,linux/386,linux/arm64,linux/arm/v7,linux/arm/v6"
+DATE=$(date '+%Y-%m-%dT%H:%M:%S')
+
+if [ -z "$GITHUB_SHA" ]; then
+echo "Guessing GITHUB_SHA"
+GITHUB_SHA=$(git rev-parse HEAD)
+fi
+
+
+# This script is only meant to run on main in github
+docker buildx build --platform "$PLATFORMS" . \
+    --target=dev \
+    --tag "$DOCKER_REPO:latest" \
+    --tag "$DOCKER_REPO:${GITHUB_SHA:0:7}" \
+    --label "org.opencontainers.image.revision=$COMMIT" \
+    --label "org.opencontainers.image.created=$DATE" \
+    --label "org.opencontainers.image.source=https://github.com/cendyne/little-queue" \
+    --push
+docker buildx build --platform "$PLATFORMS" . \
+    --target=core \
+    --tag $DOCKER_REPO:$TAGNAME \
+    --tag "$DOCKER_REPO:latest" \
+    --tag "$DOCKER_REPO:${GITHUB_SHA:0:7}" \
+    --label "org.opencontainers.image.revision=$COMMIT" \
+    --label "org.opencontainers.image.created=$DATE" \
+    --label "org.opencontainers.image.source=https://github.com/cendyne/little-queue" \
+    --push
